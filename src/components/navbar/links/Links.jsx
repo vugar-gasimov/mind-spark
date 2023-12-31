1eca402ea35a6455a8cbc77974ceda872554ef47
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-31 05:19:56 +0200

    Logic for session, admin and mobile links.

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 const Links = () => {
@@ -20,11 +22,32 @@ const Links = () => {
       path: "/blog",
     },
   ];
+  const [open, setOpen] = useState(false);
+  const session = true;
+  const isAdmin = true;
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button onClick={() => setOpen((prev) => !prev)}>Menu</button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
