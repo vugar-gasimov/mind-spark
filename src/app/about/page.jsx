414c7bd3af8f4d748b8dad6919a260b94415b558
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-01 02:49:35 +0200

    Update of about page for just bg screen with info,  update footer info.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,39 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>Digital Solutions at Their Best</h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <dic className={styles.box}>
+            <h2>1 K+</h2>
+            <p>Year of experience.</p>
+          </dic>
+          <dic className={styles.box}>
+            <h2>60 K+</h2>
+            <p>People reached</p>
+          </dic>
+          <dic className={styles.box}>
+            <h2>85 K+</h2>
+            <p>Services and Plugins</p>
+          </dic>
+        </div>
+      </div>
+
       <div className={styles.imgContainer}>
-        <Image src="/about.png" fill alt="Bacground img of about page." />
+        <Image
+          src="/about.png"
+          fill
+          alt="Bacground img of about page."
+          className={styles.img}
+        />
       </div>
     </div>
   );
