0ee1b60ec4af1f0e165ed23a635bc57c8dcc2a53
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-01 03:20:51 +0200

    New contact css file and styles for big screen, in contact page file layout with info.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,33 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="Contact image"
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and surname" />
+          <input type="email" placeholder="Email address" />
+          <input type="tel" placeholder="Phone number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+      {/* <div className={styles.container}></div> */}
+    </div>
+  );
 };
 export default ContactPage;
