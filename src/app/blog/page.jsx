269d9ca1ea1046cf7687780e0d6ba15dd0c58b84
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-01 04:31:47 +0200

    New post card and blog css files, blog page basic data layout,  card post file layout with default info.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,4 +1,21 @@
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+    </div>
+  );
 };
 export default BlogPage;
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import styles from "./postCard.module.css";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/13926757/pexels-photo-13926757.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            className={styles.img}
+          />
+        </div>
+        <span className={styles.date}>01.01.2024</span>
+      </div>
+      <div className={styles.bottom}>
+        <h2 className={styles.title}>Title</h2>
+        <p className={styles.desc}>
+          In a chic and inviting space, the café boasts an elegant black and
+          white theme. The polished bar counter stands as the centerpiece,
+          adorned with gleaming coffee cups and a modern coffee machine.
+          Reflective mirrors tastefully complement the ambiance, while
+          meticulously arranged plates add a touch of sophistication to this
+          charming setting.
+        </p>
+        <Link className={styles.link} href="/blog/post">
+          Read more
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default PostCard;
