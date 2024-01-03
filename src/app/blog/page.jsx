66aa2b1e9f3c20639deca292fc2109c7814f71f0
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-03 23:19:04 +0200

    Fix warning in blog.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,6 +1,5 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
-import { getPosts } from "@/lib/data";
 
 const getData = async () => {
   const res = await fetch("http://localhost:3000/api/blog", {
@@ -14,7 +13,7 @@ const getData = async () => {
 
 const BlogPage = async () => {
   const posts = await getData();
-  // const posts = await getPosts();
+
   return (
     <div className={styles.container}>
       {posts.map((post) => (
