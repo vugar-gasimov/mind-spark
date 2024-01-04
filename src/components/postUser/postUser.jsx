6e148fc04aa50cd7efb87988d59419719a231955
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-04 02:12:42 +0200

    Fic complite id in post User

--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,8 +1,8 @@
 import { getUser } from "@/lib/data";
 import styles from "./postUser.module.css";
 import Image from "next/image";
-const PostUser = async ({ id }) => {
-  const user = await getUser(id);
+const PostUser = async ({ userId }) => {
+  const user = await getUser(userId);
   return (
     <div className={styles.container}>
       <Image
